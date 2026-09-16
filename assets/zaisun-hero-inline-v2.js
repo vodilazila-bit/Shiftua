2bb56000bef6e50004c42dd900df7387a0f1dc9c
vodilazila-bit <vodilazila@gmail.com>
2026-09-16 10:07:57 +0300

    Update services headline copy

--- assets/zaisun-hero-inline-v2.js
+++ assets/zaisun-hero-inline-v2.js
@@ -1,1 +1,5 @@
 window.__ZAI_HERO='https://image.thum.io/get/width/1704/crop/848/noanimate/https://zaisun.com.ua/';
+(()=>{
+  const title=document.querySelector('#services .section-title');
+  if(title) title.innerHTML='Створення.<br><em>Запуск.</em><br>Підтримка магазину — від 6 місяців.';
+})();
